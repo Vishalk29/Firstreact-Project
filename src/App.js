33d3ff0d49c1610e4c099c3a2dc1971c23da4ba4
Vishal k <vkulkarni149@gmail.com>
2021-12-19 18:50:09 +0530

    first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import {CardList} from './component/card-list/card-list.component.jsx'
+import {Search} from './component/search/search.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: [],
+      searchFiled: '',
+    }
+  }
+
+  // featching the data from json placeholder
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((user) => this.setState({monsters: user}))
+  }
+  clickchange = (e) => {
+    this.setState({searchFiled: e.target.value})
+  }
+
+  render() {
+    // filtering the searchbox and calling in cardlist
+    const {monsters, searchFiled} = this.state
+    const filtered = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    )
+    // writing reusable code for search
+    return (
+      <div className='App'>
+        <h1>Monster Roledex</h1>
+        <Search placeholder='Search name' clickchange={this.clickchange} />
+        <CardList monsterss={filtered} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
